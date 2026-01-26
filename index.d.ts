82e555e3fe20063e6d67f74d889b6226ee0cefb3
Kris Powers <85710701+KrisPowers@users.noreply.github.com>
2026-01-25 21:18:32 -0500

    TypeScript index added, package.json files filtered

---
+++ index.d.ts
@@ -0,0 +1,11 @@
+/*!
+ * Triva
+ * Copyright (c) 2026 Kris Powers
+ * License MIT
+ */
+
+// Re-export everything from the main module
+export * from './lib/triva.main.js';
+
+// Re-export everything from the log module
+export * from './lib/triva.log.js';
